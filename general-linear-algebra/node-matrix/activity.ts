1ceec7c02f6081048b274a9b8e346b579c8fb147
zmineroff <zmineroff@cmu.edu>
2019-06-24 17:22:08 -0400

    Add check for row correctness

--- general-linear-algebra/node-matrix/activity.ts
+++ general-linear-algebra/node-matrix/activity.ts
@@ -43,14 +43,33 @@ const activity: Activity<State> = {
         }
       }
     }
-    const n = parseFloat(state.answer.get([0, 0]));
-    const n2 = parseFloat(state.answer.get([0, 1]));
 
-    if (n == n2) {
-      return ["justright"];
+    const correctM = math.matrix([[23/24,  -1/2,     0,  -1/8],
+                                  [  1/2,  -3/4,   1/4,     0],
+                                  [    0,   1/4,  -3/4,   1/2],
+                                  [  1/8,     0,   1/2,  -9/8]]);
+
+    const floatCompTolerance = 0.001;
+    for (let iRow = 0; iRow < nLeftRows; iRow++) {
+      let firstColResp = state.answer.get([iRow, 0]);
+      let scalar = firstColResp / correctM.get([iRow,0]);
+      for (let iCol = 1; iCol < nLeftCols; iCol++) {
+        let correctByScalar = correctM.get([iRow,iCol]) * scalar;
+        let value = state.answer.get([iRow, iCol]);
+        let valueCorrect = Math.abs(correctByScalar - value) < floatCompTolerance;
+
+        if (!valueCorrect) {
+          console.log("firstColResp: " + firstColResp);
+          console.log("scalar: " + scalar);
+          console.log("correctByScalar: " + correctByScalar);
+          console.log("value: " + value);
+          console.log(iCol);
+          return ["row"+iRow+"wrong"];
+        }
+      }
     }
 
-    return ["justright"];
+    return ["correct"];
   }
 };
 
@@ -58,7 +77,8 @@ export default activity;
 
 
 
-// TODO: handle fractions; 2nd answer https://stackoverflow.com/questions/7142657/convert-fraction-string-to-decimal
+// TODO: handle fractions (if so, don't use number type on input)
+// 2nd answer https://stackoverflow.com/questions/7142657/convert-fraction-string-to-decimal
 function readMatrixInterface(nRows: number, nCols: number) {
   var m = math.matrix();
   m.resize([nRows, nCols], undefined);
