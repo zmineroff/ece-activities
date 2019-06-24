f3fd52b467aec400ef2f8153e604ebb8795457f9
zmineroff <zmineroff@cmu.edu>
2019-06-24 14:06:25 -0400

    Add read matrix interface function

--- general-linear-algebra/node-matrix/activity.ts
+++ general-linear-algebra/node-matrix/activity.ts
@@ -2,6 +2,9 @@ import { QuestionData, Activity, ParseResponse } from "@calculemus/oli-hammock";
 import * as widgets from "@calculemus/oli-widgets";
 import * as math from 'mathjs';
 
+const nLeftRows = 4;
+const nLeftCols = 4;
+
 interface State {
     answer: math.Matrix;
     hint: widgets.HintData;
@@ -16,12 +19,12 @@ const activity: Activity<State> = {
       hint: widgets.emptyHint
     }),
     read: (): State => ({
-        answer: math.matrix(readMatrixValues()),
+        answer: math.matrix(readMatrixInterface(nLeftRows,nLeftCols)),
         hint: widgets.readHint($("#hint"))
     }),
     render: (data: QuestionData<State>): void => {
         $("#prompt").html(data.prompt!);
-        $("#answer_1_1").val(data.state.answer.get([0,0]));
+        $("#answer_0_0").val(data.state.answer.get([0,0]));
         $("#hint")
             .empty()
             .append(widgets.hint(data.hints!, data.state.hint));
@@ -30,7 +33,7 @@ const activity: Activity<State> = {
             .append(widgets.feedback(data.parts[0].feedback));
     },
     parse: (state: State): [ParseResponse] => {
-        if (state.answer.get([0,0]) === undefined) return [null];
+        if (state.answer.get([0,0]) === undefined) return ["nan"];
         const n = parseFloat(state.answer.get([0,0]));
         if (isNaN(n)) return ["nan"];
         if (n < 42) return ["small"];
@@ -42,15 +45,26 @@ const activity: Activity<State> = {
 export default activity;
 
 
-function readMatrixValue(inputID) {
-  return parseFloat(`${$(inputID).val()}`);
-}
 
-function readMatrixValues() {
-  const m = math.matrix([[readMatrixValue("#answer_1_1"),undefined,undefined,undefined],
-               [undefined, undefined, undefined, undefined],
-               [undefined, undefined, undefined, undefined],
-               [undefined, undefined, undefined, undefined]])
+function readMatrixInterface(nRows, nCols) {
+  var m = math.matrix();
+  m.resize([nRows, nCols], undefined);
+
+  for (let iRow=0; iRow<nRows; iRow++) {
+    for (let iCol=0; iCol<nCols; iCol++) {
+      let inputId = "#answer_" + iRow + "_" + iCol;
+      let value = parseFloat(`${$(inputId).val()}`);
+
+      console.log(inputId);
+      console.log(value);
+      if (isNaN(value)) {
+        console.log('hoi');
+        value = undefined;
+      }
+
+      m.subset(math.index(iRow, iCol),value);
+    }
+  }
 
   return m;
 }
