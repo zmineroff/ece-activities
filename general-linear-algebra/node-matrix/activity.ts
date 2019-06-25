d6324a182dae026ae103f243beddb4c04e990e95
zmineroff <zmineroff@cmu.edu>
2019-06-25 00:38:07 -0400

    Cleanup

--- general-linear-algebra/node-matrix/activity.ts
+++ general-linear-algebra/node-matrix/activity.ts
@@ -20,22 +20,29 @@ const activity: Activity<State> = {
     hint: widgets.readHint($("#hint"))
   }),
   render: (data: QuestionData<State>): void => {
+    // Prompt
     $("#prompt").html(data.prompt!);
+
+    // Matrix inputs
     for (let iRow = 0; iRow < nRows; iRow++) {
       for (let iCol = 0; iCol < nCols; iCol++) {
         let inputId = "#answer_" + iRow + "_" + iCol;
         $(inputId).val(data.state.answer.get([iRow, iCol]));
       }
     }
+
+    // Hints
     $("#hint")
       .empty()
       .append(widgets.hint(data.hints!, data.state.hint));
+
+    // Feedback
     $("#feedback")
       .empty()
       .append(widgets.feedback(data.parts[0].feedback));
   },
   parse: (state: State): [ParseResponse] => {
-    // check that a number is entered in all cells
+    // Check that a number is entered in all cells
     for (let iRow = 0; iRow < nRows; iRow++) {
       for (let iCol = 0; iCol < nCols; iCol++) {
         let value = state.answer.get([iRow, iCol]);
@@ -45,12 +52,12 @@ const activity: Activity<State> = {
       }
     }
 
+    // Check that each row of answer is a scalar multiple of correct matrix
     const correctMatrix = math.matrix([[23/24,  -1/2,     0,  -1/8,  3],
                                        [  1/2,  -3/4,   1/4,     0,  0],
                                        [    0,   1/4,  -3/4,   1/2,  0],
                                        [  1/8,     0,   1/2,  -9/8,  0]]);
 
-    //const floatCompTolerance = 0.01;
     for (let iRow = 0; iRow < nRows; iRow++) {
       let submittedVector = getRowAsVector(state.answer, iRow);
       let correctVector = getRowAsVector(correctMatrix, iRow);
@@ -70,6 +77,7 @@ export default activity;
 
 // TODO: handle fractions (if so, don't use number type on input)
 // 2nd answer https://stackoverflow.com/questions/7142657/convert-fraction-string-to-decimal
+// can also use math.fraction
 function readMatrixInterface(nRows: number, nCols: number) {
   var m = math.matrix();
   m.resize([nRows, nCols], undefined);
@@ -89,33 +97,38 @@ function readMatrixInterface(nRows: number, nCols: number) {
   return m;
 }
 
-// return true if vector2 is a scalar multiple of vector 1
-// return false otherwise
+// Returns true if vector2 is a scalar multiple of vector1, false otherwise
 function isScalarMultiple(vector1: math.Matrix, vector2: math.Matrix) {
   console.log("Vec1: " + vector1);
   console.log("Vec2: " + vector2);
-  let len = math.max(math.size(vector1));
-  console.log("len: " + len);
+
+  // Check that lengths are equal
+  const len = math.max(math.size(vector1));
   if (len!==math.max(math.size(vector2))) {
     return false;
   }
 
-  let zeroTolerance = 0.001;
-  let valueTolerance = 0.01;
+  const zeroTolerance = 0.0001;
+  const valueTolerance = 0.01;
 
   let bothAllZero = true;
   let scalar = null;
   for (let i=0; i<len; i++) {
     let val1 = vector1.get([i]);
     let val2 = vector2.get([i]);
+
     let val1zero = floatsEquivalent(val1,0,zeroTolerance);
     let val2zero = floatsEquivalent(val2,0,zeroTolerance);
     if (val1zero !== val2zero) {
+      // one value is zero, but not the other
       return false;
     }
+
     if (bothAllZero && (!val1zero || !val2zero)) {
+      // at least one value is non-zero
       bothAllZero = false;
     }
+
     if (scalar===null
         && !val1zero
         && !val2zero) {
@@ -134,6 +147,7 @@ function isScalarMultiple(vector1: math.Matrix, vector2: math.Matrix) {
   }
 
   if (scalar===null) {
+    // one vector is all zeros, the other isn't
     return false;
   }
 
@@ -141,14 +155,14 @@ function isScalarMultiple(vector1: math.Matrix, vector2: math.Matrix) {
 }
 
 
-//return true if two floats are equivalent, within tolerance
+// Returns true if two floats are equivalent, within tolerance
 function floatsEquivalent(float1: number, float2: number, tolerance: number) {
   return Math.abs(float1 - float2) < tolerance;
 }
 
-// returns row from matrix as a vector
+// Returns row from matrix as a vector
 function getRowAsVector(m: math.Matrix, row: number) {
-  let rowMatrix = (math as any).row(m, row);
+  let rowMatrix = (math as any).row(m, row); //row not defined in types???
   let len = math.max(math.size(rowMatrix));
   let rowVector = math.reshape(rowMatrix, [len]);
   return rowVector as math.Matrix;
