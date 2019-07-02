ad908e7b93b837ba2ca95deb3b83959376b4aa89
zmineroff <zmineroff@cmu.edu>
2019-07-02 12:46:37 -0400

    Fix style and feedback

--- general-linear-algebra/select-nodes/activity.ts
+++ general-linear-algebra/select-nodes/activity.ts
@@ -2,19 +2,26 @@ import { QuestionData, Activity, ParseResponse } from "@calculemus/oli-hammock";
 import * as widgets from "@calculemus/oli-widgets";
 
 interface State {
-    answer: string;
+    answer: string[];
     hint: widgets.HintData;
 }
 
 const activity: Activity<State> = {
-    init: (): State => ({ answer: "", hint: widgets.emptyHint }),
+    init: (): State => ({ answer: [], hint: widgets.emptyHint }),
     read: (): State => ({
-        answer: `${$("#answer").val()}`,
+        answer: readAnswerFromInterface(),
         hint: widgets.readHint($("#hint"))
     }),
     render: (data: QuestionData<State>): void => {
         $("#prompt").html(data.prompt!);
-        $("#answer").val(data.state.answer);
+        $("input:checkbox[name=answer]").each(function(){
+            let inputValue = `${$(this).val()}`;
+            if (data.state.answer.includes(inputValue)) {
+                $(this).prop("checked", true);
+            } else {
+                $(this).prop("checked", false);
+            }
+        });
         $("#hint").empty().append(widgets.hint(data.hints!, data.state.hint));
         $("#feedback")
             .empty()
@@ -22,13 +29,39 @@ const activity: Activity<State> = {
         $("#circuit-image").attr("src", data.config.circuitImg);
     },
     parse: (state: State): [ParseResponse] => {
-        if (state.answer.trim() === "") return [null];
-        const n = parseInt(state.answer);
-        if (isNaN(n)) return ["nan"];
-        if (n < 42) return ["small"];
-        if (n > 42) return ["big"];
-        return ["justright"];
+        if (state.answer === []) return [null];
+
+        let selectedVoltageNode = state.answer.includes("A") || state.answer.includes("E");
+        if (selectedVoltageNode) {
+            return ["selectedVoltageNode"];
+        }
+
+        let selectedIrrelevantNode = state.answer.includes("D") || state.answer.includes("H");
+        if (selectedIrrelevantNode) {
+            return ["selectedIrrelevantNode"]
+        }
+
+        let allCorrectSelected = state.answer.includes("B")
+                                 && state.answer.includes("C")
+                                 && state.answer.includes("F")
+                                 && state.answer.includes("G");
+
+        if (allCorrectSelected) {
+            return ["correct"];
+        } else {
+            return["selectedSubsetCorrect"];
+        }
     }
 };
 
 export default activity;
+
+
+function readAnswerFromInterface() {
+    let answerArray = []
+    $("input:checkbox[name=answer]:checked").each(function(){
+        answerArray.push($(this).val());
+    });
+
+    return answerArray;
+}
