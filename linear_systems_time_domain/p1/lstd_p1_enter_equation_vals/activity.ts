454ade9f9573f0f3c80dea012fdb1c5e8943e7fc
zmineroff <zmineroff@cmu.edu>
2019-07-28 16:26:12 -0400

    Setup layout inputs

--- linear_systems_time_domain/p1/lstd_p1_enter_equation_vals/activity.ts
+++ linear_systems_time_domain/p1/lstd_p1_enter_equation_vals/activity.ts
@@ -2,27 +2,44 @@ import { QuestionData, Activity, ParseResponse } from "@calculemus/oli-hammock";
 import * as widgets from "@calculemus/oli-widgets";
 
 interface State {
-    answer: string;
+    diffeq_var_1: string;
+    coeff_linear_term: string;
+    diffeq_var_2: string;
+    forcing_function: string;
     hint: widgets.HintData;
 }
 
 const activity: Activity<State> = {
-    init: (): State => ({ answer: "", hint: widgets.emptyHint }),
+    init: (): State => ({ 
+        diffeq_var_1: "",
+        coeff_linear_term: "",
+        diffeq_var_2: "",
+        forcing_function: "",
+        hint: widgets.emptyHint
+    }),
     read: (): State => ({
-        answer: `${$("#answer").val()}`,
+        diffeq_var_1: `${$("#diffeq_var_1").val()}`,
+        coeff_linear_term: `${$("#coeff_linear_term").val()}`,
+        diffeq_var_2: `${$("#diffeq_var_2").val()}`,
+        forcing_function: `${$("#forcing_function").val()}`,
         hint: widgets.readHint($("#hint"))
     }),
     render: (data: QuestionData<State>): void => {
         $("#prompt").html(data.prompt!);
-        $("#answer").val(data.state.answer);
+
+        $("#diffeq_var_1").val(data.state.diffeq_var_1);
+        $("#coeff_linear_term").val(data.state.coeff_linear_term);
+        $("#diffeq_var_2").val(data.state.diffeq_var_2);
+        $("#forcing_function").val(data.state.forcing_function);
+
         $("#hint").empty().append(widgets.hint(data.hints!, data.state.hint));
         $("#feedback")
             .empty()
             .append(widgets.feedback(data.parts[0].feedback));
     },
     parse: (state: State): [ParseResponse] => {
-        if (state.answer.trim() === "") return [null];
-        const n = parseInt(state.answer);
+        if (state.diffeq_var_1.trim() === "") return [null];
+        const n = parseInt(state.diffeq_var_1);
         if (isNaN(n)) return ["nan"];
         if (n < 42) return ["small"];
         if (n > 42) return ["big"];
