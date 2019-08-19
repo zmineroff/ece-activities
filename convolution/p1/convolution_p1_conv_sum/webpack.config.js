754e2f01ae4b7f2e0d8e90f4e763251536d8ec22
zmineroff <zmineroff@cmu.edu>
2019-08-19 16:38:48 -0400

    Create convolution problem 1
    Create a new hammock project for convolution problem 1

---
+++ convolution/p1/convolution_p1_conv_sum/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        activity: "./main.ts"
+    },
+    output: {
+        filename: "activity.js",
+        libraryTarget: "umd",
+        path: path.resolve(__dirname, "dist")
+    },
+    devServer: {
+        contentBase: [
+            path.join(__dirname),
+            path.join(__dirname, "node_modules/@calculemus/oli-hammock/assets"),
+        ],
+        port: 8080
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [ { test: /.ts$/, loader: "ts-loader"} ]
+    },
+    externals: {
+    },
+}
