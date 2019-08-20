5af68440333e184d7c5478a6a867814de8f42811
zmineroff <zmineroff@cmu.edu>
2019-08-20 14:23:07 -0400

    Add basic feedback for convolution p1

--- convolution/p1/convolution_p1_conv_sum/activity.ts
+++ convolution/p1/convolution_p1_conv_sum/activity.ts
@@ -2,28 +2,47 @@ import { QuestionData, Activity, ParseResponse } from "@calculemus/oli-hammock";
 import * as widgets from "@calculemus/oli-widgets";
 
 interface State {
-    answer: string;
+    x_expression: string;
+    h_expression: string;
 }
 
 const activity: Activity<State> = {
-    init: (): State => ({ answer: "" }),
+    init: (): State => ({
+        x_expression: "",
+        h_expression: ""
+    }),
     read: (): State => ({
-        answer: `${$("#answer").val()}`
+        x_expression: `${$("#x_expression").val()}`,
+        h_expression: `${$("#h_expression").val()}`
     }),
     render: (data: QuestionData<State>): void => {
         $("#prompt").html(data.prompt!);
-        $("#answer").val(data.state.answer);
+
+        // Need to use .attr('value', value) instead of .val(value) for mathml inputs
+        $("#x_expression").attr('value', data.state.x_expression);
+        $("#h_expression").attr('value', data.state.h_expression);
+
         $("#feedback")
             .empty()
             .append(widgets.feedback(data.parts[0].feedback));
     },
     parse: (state: State): [ParseResponse] => {
-        if (state.answer.trim() === "") return [null];
-        const n = parseInt(state.answer);
-        if (isNaN(n)) return ["nan"];
-        if (n < 42) return ["small"];
-        if (n > 42) return ["big"];
-        return ["justright"];
+        let x_resp = state.x_expression.replace(/\s/g,'');
+        let h_resp = state.h_expression.replace(/\s/g,'');
+
+        if (x_resp === "" || h_resp === "") {
+                return ["nan"];
+        }
+
+        if (x_resp.toUpperCase() !== "K") {
+            return ["x_resp_wrong"];
+        }
+
+        if (h_resp.toUpperCase() !== "N-K") {
+            return ["h_resp_wrong"];
+        }
+
+        return ["correct"];
     }
 };
 
